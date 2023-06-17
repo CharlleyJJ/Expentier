4a70e51d85f698ab4465f9fd9318970d3e13fea7
CharlleyJJ <cjjabbar_@hotmail.com>
2023-06-17 19:23:59 -0300

    added Expenses options - btns states

--- app/page.js
+++ app/page.js
@@ -1,10 +1,21 @@
 
-export default function Home() {
-  return <section>
-<small className="text-gray-400 text-md">
-  My Balance
-</small>
+import {currencyFormatter} from '@/lib/utils';
 
-<h2 className="text-4xl font-bold">R$ 1237.00</h2>
-  </section>
+
+
+export default function Home() {
+  return (
+    <main className="container max-w-2xl px-6 mx-auto">
+      <section className="py-3">
+        <small className="text-gray-400 text-md">My Balance</small>
+
+        <h2 className="text-4xl font-bold"> {currencyFormatter(200000)} </h2>
+      </section>
+
+      <section className="flex gap-2 items-center py-3">
+        <button className="btn btn-primary"> - Expenses</button>
+        <button className="btn btn-primary-outline"> + Income</button>
+      </section>
+    </main>
+  );
 }
