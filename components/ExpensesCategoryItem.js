e54741d814e965271eae3ee4b96c4c50cd4626f0
CharlleyJJ <cjjabbar_@hotmail.com>
2023-08-16 09:50:06 -0300

    Added Expenses Category Item

--- components/ExpensesCategoryItem.js
+++ components/ExpensesCategoryItem.js
@@ -1,20 +1,36 @@
+import { useState } from "react";
+
 import { currencyFormatter } from "@/lib/utils";
 
-function ExpenseCategoryItem({ title, total, color }) {
+import ViewExpenseModal from "./modals/ViewExpensesModal";
+
+
+function ExpenseCategoryItem({ expense }) {
+ const [showViewExpenseModal, setViewExpenseModal] = useState(false);
+ 
   return (
-    <button>
+    <>
+      <ViewExpenseModal 
+      show={showViewExpenseModal} 
+      onClose={setViewExpenseModal}
+      expense={expense}
+    />
+      <button onClick={() => { setViewExpenseModal(true);
+    }}
+    >
       <div className="flex items-center justify-between px-4 py-3  bg-gradient-to-tr  from-gray-700 via-gray-900 to-black rounded-3xl  hover:border transition hover:border-slate-700  duration-700 ease-in-out hover:scale-105 ">
         <div className="flex items-center gap-2">
           {/*Circle Div, Could be an Icon*/}
           <div
             className="w-[25px] h-[25px] rounded-full bg-slate-500"
-            style={{ backgroundColor: color }}
+            style={{ backgroundColor: expense.color }}
           />
-          <h4 className="capitalize">{title}</h4>
+          <h4 className="capitalize">{expense.title}</h4>
         </div>
-        <p>{currencyFormatter(total)}</p>
+        <p>{currencyFormatter(expense.total)}</p>
       </div>
     </button>
+    </>
   );
 }
 
