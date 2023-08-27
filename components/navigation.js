cbfffcbceb7e6abd093af9359dcb506711d6c542
CharlleyJJ <cjjabbar_@hotmail.com>
2023-08-27 05:26:08 -0300

    Added Auth Functions to home

--- components/navigation.js
+++ components/navigation.js
@@ -1,33 +1,48 @@
 
 import {ImStatsBars} from 'react-icons/im'
+import { useContext } from 'react';
+import { authContext } from '@/lib/store/auth-context.js';
 
 
 function Nav() {
+
+  
+  const values =  useContext(authContext);
+  const {user,loading,logout} = values;
+ 
+
     return ( 
     <header className='container max-w-2xl p-6 mx-auto' >
     <div className="flex items-center justify-between">
       {/*User Information*/}
-      <div className="flex items-center gap-2">
+      {user && !loading && (
+        <div className="flex items-center gap-2">
         <div className="h-[40px] w-[40px] rounded-full overflow-hidden">
             {/*User Photo*/}
         <img 
         className="w-full object-cover" 
-        src="https://charlleyjj-portfolio-2023.vercel.app/assets/Cjj_About-6acaf09f.jpeg" 
-        alt="Profile Image"/>
+        src={user.photoURL}
+        alt={user.displayName}
+        referrerPolicy="no-referrer"/>
+
         </div>
          {/*User Name*/}
-        <small>Hi Charlley JJ</small>
+        <small>Hi {user.displayName}</small>
       </div>
+      )}
+     
          {/*Icon and Button*/}
+         {user && !loading && (
       <nav className="flex items-center gap-4">
         <div>
           <ImStatsBars className='text-2xl'/>
         </div>
 
         <div>
-          <button className='btn btn-danger'>Log off</button>
+          <button onClick={logout} className='btn btn-danger'>Log off</button>
         </div>
       </nav>
+       )}
     </div>
   </header>
   );
